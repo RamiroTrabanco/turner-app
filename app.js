1b508518db3eb2101095913e29d252ba8165e74e
RamiroTrabanco <trabancoramiro@gmail.com>
2023-12-05 18:28:49 -0300

    arreglo port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const readline = require('readline');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 
 
 app.use(express.urlencoded({ extended: true }));
