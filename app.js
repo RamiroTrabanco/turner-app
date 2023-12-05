ccf50bcf61fec3399df3ff65cf0d6c9e67ce879b
RamiroTrabanco <trabancoramiro@gmail.com>
2023-12-05 18:06:22 -0300

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const readline = require('readline');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
