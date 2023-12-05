7d0879928f9fdedf0566cb148fb2ee4b2e925604
RamiroTrabanco <trabancoramiro@gmail.com>
2023-12-05 16:23:48 -0300

    arreglo style y agrego creditos

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public', { 'extensions': ['css'] }));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
